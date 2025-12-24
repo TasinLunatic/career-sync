e0822bdd19904bc5946b2652b59e444c7cfb3ec4
TasinLunatic <ahmedtasin207@gmail.com>
2025-12-24 17:28:40 +0800

    design login page by using shadcn

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AppRouter />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>,
 );
